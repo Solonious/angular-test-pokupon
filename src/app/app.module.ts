c25e74df3a7fbfb36f9541fdd63419c615032cc4
Sergey Solonar <s.solonar@superdeal.com.ua>
2017-11-03 11:27:04 +0200

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {Material2Module} from './material2/material2.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,9 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Material2Module
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
